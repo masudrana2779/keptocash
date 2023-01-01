9087493160d186c1c9ff86e7e723b979da7637c4
masudrana2779 <masudrana2779@gmail.com>
2023-01-01 22:19:44 +0600

    banner slider try to done

--- components/Banner/banner.styled.tsx
+++ components/Banner/banner.styled.tsx
@@ -6,22 +6,35 @@ export const BannerWrap = styled.div`
   .slick-slide {
     transition: all 0.5s;
     &.slick-current {
-      transform: scale(1.1);
+    }
+  }
+
+  .slick-arrow {
+    z-index: 1;
+    &.slick-prev {
+      left: 110px;
+    }
+    &.slick-next {
+      right: 110px;
     }
   }
 
   .slick-dots {
-    bottom: -10px;
+    bottom: 50px;
     li {
-      width: 6px;
-      height: 6px;
+      width: 8px;
+      height: 8px;
       border-radius: 50%;
-      background: #a9b2b6;
-      margin: 0 2px;
+      background: #033a40;
+      margin: 0 5px;
       transition: all 0.5s;
+      border: 1px solid transparent;
+      box-shadow: 0px 0px 4px transparent;
       &.slick-active {
         transform: scale(1.5);
-        background: #498553;
+        background: #0093a4;
+        border: 1px solid #4ec0c7;
+        box-shadow: 0px 0px 4px #00e6ff;
       }
       button {
         display: none;
--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Slider from "react-slick";
 import { BannerWrap } from "./banner.styled";
 
 const BannerComponent = () => {
@@ -8,7 +9,7 @@ const BannerComponent = () => {
     speed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     centerMode: true,
     centerPadding: "40px",
     autoplay: true,
@@ -23,43 +24,45 @@ const BannerComponent = () => {
         }}
         className=""
       >
-        <BannerWrap className="pt-[120px] pb-24">
-          {/* <Slider {...settings}>
+        <BannerWrap className="">
+          <Slider {...settings}>
             {[1, 1, 1, 1].map((item, i: number) => (
-              
+              <>
+                <div className="container mx-auto px-40">
+                  <div className="flex justify-between items-center gap-x-4 pt-[120px] pb-24">
+                    <div className="max-w-xs w-full">
+                      <div className="text-brand-600 text-base leading-6 font-semibold mb-2">
+                        Cashless Payment
+                      </div>
+                      <div className="text-5xl font-bold text-white leading-[64px] mb-3">
+                        Easy Solution
+                      </div>
+                      <div className="text-base leading-7 text-white mb-4">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem.
+                      </div>
+                      <div className="text-sm leading-6 text-brand-600">
+                        Learn More →
+                      </div>
+                    </div>
+                    <div className="">
+                      <div className="flex flex-shrink-0">
+                        <Image
+                          src={"/assets/images/bannerImg.png"}
+                          className={"flex-shrink-0"}
+                          width={632}
+                          height={509}
+                          alt="bannerImg.png"
+                          placeholder="blur"
+                          blurDataURL="/assets/images/bannerImg.png"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
             ))}
-          </Slider> */}
-          <div className="container mx-auto px-40">
-            <div className="flex justify-between items-center gap-x-4">
-              <div className="max-w-xs w-full">
-                <div className="text-brand-600 text-base leading-6 font-semibold mb-2">
-                  Cashless Payment
-                </div>
-                <div className="text-5xl font-bold text-white leading-[64px] mb-3">
-                  Easy Solution
-                </div>
-                <div className="text-base leading-7 text-white mb-4">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem.
-                </div>
-                <div className="text-sm leading-6 text-brand-600">
-                  Learn More →
-                </div>
-              </div>
-              <div className="">
-                <div className="flex">
-                  <Image
-                    src={"/assets/images/bannerImg.png"}
-                    width={632}
-                    height={509}
-                    alt="bannerImg.png"
-                    placeholder="blur"
-                    blurDataURL="/assets/images/bannerImg.png"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+          </Slider>
         </BannerWrap>
       </div>
     </>
